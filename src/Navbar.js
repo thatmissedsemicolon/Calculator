595b6e365b7dee014ff70363f9e107e46a9ff942
SURYANSH <77827345+Suryansh954@users.noreply.github.com>
2022-04-11 00:13:53 -0400

    changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
            <img className="w-9 h-9 float-right" src={Image}/>
            Calculator 
         </p>
-      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Dark'}</button>
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? 'Light Mode' : 'Dark Mode'}</button>
     </div>
     <Cal/>
   </div>
